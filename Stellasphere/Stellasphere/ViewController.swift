9ce53dcab69aa3ec8633e058a893c0e889bcf092
kircherm824 <kircherm824@nicc.edu>
2026-03-25 10:00:49 -0500

    Add files via upload

---
+++ Stellasphere/Stellasphere/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stellasphere
+//
+//  Created by Programmer on 3/25/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Stellasphere/StellasphereTests/StellasphereTests.swift
@@ -0,0 +1,17 @@
+//
+//  StellasphereTests.swift
+//  StellasphereTests
+//
+//  Created by Programmer on 3/25/26.
+//
+
+import Testing
+@testable import Stellasphere
+
+struct StellasphereTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
