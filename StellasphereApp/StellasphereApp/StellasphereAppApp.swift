041ef6c97bbf6b890119208a614f0f43580cb15d
kircherm824 <kircherm824@nicc.edu>
2026-03-25 09:26:16 -0500

    Add files via upload
    redone in storyboard

---
+++ StellasphereApp/StellasphereApp/StellasphereAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StellasphereAppApp.swift
+//  StellasphereApp
+//
+//  Created by Programmer on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct StellasphereAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
